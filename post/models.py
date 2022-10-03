342e59da8d77a5ea06d54895f7ecd5a0c0bffaec
이금빈 <lgb@golbyeong-deun-latte.local>
2022-10-03 17:26:09 +0900

    포스트 모델 추가
    포스트 모델 추가합니다.

--- post/models.py
+++ post/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from user.models import UserModel
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class PostModel(models.Model):
+    class Meta:
+        db_table = "post"
+
+    author     = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    # pf_image   = models.ForeignKey('Pf_image', on_delete=models.CASCADE, upload_to='timeline_photo/%Y/%m/%d/%h/%s')
+    # post_image = models.ImageField()
+    text       = models.CharField(max_length=256, blank=True)
+    like_count = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
