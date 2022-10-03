d56735d557ece86a60029c9145a18fd5a10e5a5b
이금빈 <lgb@golbyeong-deun-latte.local>
2022-10-04 04:00:34 +0900

    코멘트 모델 추가 및 간단한 수정
    1. 코멘트 모델 추가
    2. 게시글 관련 url설정해 두었고, 연결되는 함수들 views에 간단하게 생성해놓았습니다.
    3. home.html의 경우 유저와 관련된 정보와 게시물 정보가 모두 나와 user 폴더에서 전체 템플릿 폴더 내부로 이동시켰습니다.
    4. post/models.py에서 post모델의 text 필드의 max_length 수정

--- post/views.py
+++ post/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_view(request):
+    return render(request, 'post/post.html')
+
+def post_detail(request):
+    return render(request, 'post/post_detail.html')
+
+def delete_post(request):
+    pass
+
+def write_comment(request) :
+    pass
+
+def delete_comment(request) :
+    pass
