90f4ba8aba95d104399b9ec16ce32d70ec3c97ac
tweaky3047 <yy304742@gmail.com>
2022-10-06 02:51:35 +0900

    피드 상세페이지 연결 및 수정

--- user/models.py
+++ user/models.py
@@ -11,7 +11,7 @@ class UserModel(AbstractUser):
         db_table = "my_user" # 여기는 테이블 이름이에요! 꼭 기억 해 주세요!
 
     bio = models.TextField(max_length=500, blank=True)
-    profile_image = models.ImageField(upload_to='timeline_photo/%Y/%m/%d/%h/%s')
+    profile_image = models.ImageField(upload_to='timeline_photo')
     follow = models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='followee')
     nickname = models.CharField(max_length=30, blank=False, null=True)
 
