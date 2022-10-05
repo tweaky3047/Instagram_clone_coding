427450c9a28a0cbcc2747e5912b0a18d70c5fa77
tweaky3047 <yy304742@gmail.com>
2022-10-06 07:09:19 +0900

    댓글 DB연결

--- post/models.py
+++ post/models.py
@@ -5,19 +5,11 @@ from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 import django
 from django import utils
-class PostModel(models.Model):
-    class Meta:
-        db_table = "post"
 
-    author     = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    text       = models.CharField(max_length=255, blank=True)
-    # charfield의 제한이 255까지라 수정해 두었습니다.
-    like_count = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     # hashtag = models.CharField(max_length=30)
 
 class Feed(models.Model):
+    
     content = models.TextField()    # 글내용
     image = models.TextField()  # 피드 이미지
     profile_image = models.TextField()
@@ -31,9 +23,9 @@ class CommentModel(models.Model) :
     class Meta :
         db_table = "comment"
     
-    post = models.ForeignKey(PostModel, on_delete=models.CASCADE)
+    post = models.ForeignKey(Feed, on_delete=models.CASCADE)
     author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     #제가 찾은 바로는 덧글에 글자수 제한이 없어서 계획은 varchar였는데 일단은 textfield로 해놓았습니다.
-    comment = models.TextField()
+    comment = models.CharField(max_length=256)
     #제가 못 찾은 건지 덧글 수정 기능을 찾을 수 없어 생성시時만 기록하도록 auto_now_add를 사용했습니다.
     created_at = models.DateTimeField(auto_now_add=True)
