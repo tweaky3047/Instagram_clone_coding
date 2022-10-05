39d23fe33def85ac67a12f56a8dc075e626555b5
tweaky3047 <yy304742@gmail.com>
2022-10-05 10:01:17 +0900

    daily commit

--- user/views.py
+++ user/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render,redirect
 from .models import UserModel
 from django.contrib.auth import get_user_model
 # Create your views here.
+
 def home_view(request):    
     return render(request, 'home.html')
 
@@ -15,22 +16,22 @@ def sign_up(request) :
 
         return render(request, 'user/sign_up.html')
     elif request.method == 'POST':
-        email = request.POST.get('phone_number_or_email',None)
-        username = request.POST.get('user_name',None)
+        username = request.POST.get('phone_number_or_email',None)
+        last_name = request.POST.get('user_name',None)
         nickname = request.POST.get('user_id',None)
         password = request.POST.get('password',None)
 
         
-        if email == '' or password == '':
+        if username == '' or password == '':
                 return render(request, 'user/sign_up.html', {'error': '휴대폰 번호 또는 이메일과 비밀번호는 필수입니다.'})
         else:
-            exist_user =get_user_model().objects.filter(email = email)
+            exist_user =get_user_model().objects.filter(username = username)
             if exist_user:
                     return render(request, 'user/sign_up.html',{'error': '휴대폰 번호 또는 이메일이 이미 존재합니다.'})
             else:       
                 new_user = UserModel()
-                new_user.email = email
                 new_user.username = username
+                new_user.last_name = last_name
                 new_user.nickname = nickname
                 new_user.password = password
                 new_user.save()
